c4cc19e89a512172b738c0a06569e15df2c956b0
wjjj <1226808765@qq.com>
2019-02-10 14:03:03 +0800

    welcome

---
+++ action/TeacherLogin.py
@@ -0,0 +1,3 @@
+def TeacherLogin():
+    print('1.注册')
+    print('2.登录')
--- action/welcome.py
+++ action/welcome.py
@@ -1,4 +1,14 @@
-print("作业管理系统")
-print("请选择你的登录方式(输入序号以选择)：")
-print("1.老师")
-print("2.学生")
+from action import TeacherLogin,StudentLogin
+
+print('作业管理系统')
+while True:
+    print('请选择你的登录方式(输入序号以选择)：')
+    print('1.老师')
+    print('2.学生')
+    choose = input()
+    if choose == '1':
+        TeacherLogin()
+    elif choose == '2':
+        StudentLogin()
+    else:
+        print('输入错误请重新选择')
