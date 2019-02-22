0616cf1b774af19a352cc539943685265efaa1a0
wjjj <1226808765@qq.com>
2019-02-22 08:55:18 +0800

    外壳打造完成

--- Data/Data.py
+++ Data/Data.py
@@ -1,11 +1,24 @@
+import  os
 def creatFile(filename):
+    os.chdir('/Volumes/data/PycharmProject/Struct/Data')
     fileobject = open(filename, 'w')
     fileobject.close()
 
 def readFile(filename):
+    rs = []
+    os.chdir('/Volumes/data/PycharmProject/Struct/Data')
     fileoject = open(filename, 'r')
-    return fileoject.read()
+    line = fileoject.readline().strip()
+    while line:
+        linestr = line.split(',')
+        rs.append(linestr)
+        line = fileoject.readline().strip()
+    return rs
 def addItem(filename,item):
+    os.chdir('/Volumes/data/PycharmProject/Struct/Data')
     fileobject = open(filename, 'a')
-    fileobject.write(item)
+    if item != '\n':
+        fileobject.write(item + ',')
+    else:
+        fileobject.write(item)
     fileobject.close()
--- function/teacher/TeacherRegister.py
+++ function/teacher/TeacherRegister.py
@@ -1,8 +1,12 @@
 from Data import Data
-def teacherLogin():
-    name     = input('请输入你的姓名')
-    subject  = input('请输入你所任教的科目')
-    number   = input("请输入你的工号：")
-    password = input("请输入你的密码：")
-    teacherinformation = [name, subject, number, password]
-    Data.addItem('TeacherInformation', teacherinformation)
+
+name     = input('请输入你的姓名')
+subject  = input('请输入你所任教的科目')
+number   = input("请输入你的工号：")
+password = input("请输入你的密码：")
+teacherinformation = [name, subject, number, password]
+for i in teacherinformation:
+    Data.addItem('TeacherInformation', i)
+Data.addItem('TeacherInformation', '\n')
+re = Data.readFile('TeacherInformation')
+print(re)
