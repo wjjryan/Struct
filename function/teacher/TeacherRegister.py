bb8bd8637aee2d98c04dea01f555d45cb310c840
wjjj <1226808765@qq.com>
2019-02-28 12:55:13 +0800

    老师的功能完成

--- function/teacher/TeacherRegister.py
+++ function/teacher/TeacherRegister.py
@@ -1,13 +1,13 @@
 from Data import Data
-
-name     = input('请输入你的姓名')
-subject  = input('请输入你所任教的科目')
-number   = input("请输入你的工号：")
-password = input("请输入你的密码：")
-teacherinformation = [name, subject, number, password]
-for i in teacherinformation:
-    Data.addItem('TeacherInformation', i)
-Data.addItem('TeacherInformation', '\n')
-re = Data.readFile('TeacherInformation')
-for i in re:
-    print(i[1])
+def teacherRegister():
+    name     = input('请输入你的姓名')
+    subject  = input('请输入你所任教的科目')
+    number   = input("请输入你的工号：")
+    password = input("请输入你的密码：")
+    teacherinformation = [name, subject, number, password]
+    for i in teacherinformation:
+        Data.addItem('TeacherInformation', i)
+    Data.addItem('TeacherInformation', '\n')
+    re = Data.readFile('TeacherInformation')
+    for i in re:
+        print(i[1])
